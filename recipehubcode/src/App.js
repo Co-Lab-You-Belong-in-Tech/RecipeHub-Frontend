233bc248100def40fe889a80903080edc8ac8352
Jonathan Martin <oldschoolspaceempire@gmail.com>
2021-08-12 13:39:35 -0500

    making great progress on RecipeHub

--- recipehubcode/src/App.js
+++ recipehubcode/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+// import Contact from './pages/contactPage';
+// import Login from './pages/loginPage';
+// import HomePageContent from './components/homePageContent';
+// import SignUp from './pages/signUpPage';
+import { curRentState } from './assets/pageTitles';
+import './components/App.css';
 
 function App() {
+  // const [loggedIn, setLoggedIn] = useState(false);
+  const [currentState, setCurrentState] = useState({ ...curRentState });
+  console.log('my state', currentState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Router>
+        <Container className='p-0' fluid={true}>
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
+            <Navbar.Brand>RecipeHub</Navbar.Brand>
+            <Navbar.Collapse id='navbar-toggle'>
+              <Nav className='ml-auto'>
+                <Link className='nav-link' to='/'>
+                  Home
+                </Link>
+                <Link className='nav-link' to='/contact'>
+                  Contact
+                </Link>
+                <Link className='nav-link' to='/signup'>
+                  Sign Up
+                </Link>
+                <Link className='nav-link' to='/login'>
+                  Login
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Route
+            path='/'
+            exact
+            // render={() => (
+            //   <HomePageContent
+            //     title={currentState.home.title}
+            //     text={currentState.home.text}
+            //   />
+            // )}
+          />
+          <Route
+            path='/Contact'
+            // render={() => <Contact title={currentState.contact.title} text={currentState.contact.text} />}
+          />
+          <Route
+            path='/Sign Up Today!'
+            // render={() => <SignUp title={currentState.signup.title} />}
+          />
+          <Route
+            path='/Login'
+            // render={() => <Login title={currentState.login.title} />}
+          />
+          {/* <Footer /> */}
+        </Container>
+      </Router>
+    </Switch>
   );
 }
 
