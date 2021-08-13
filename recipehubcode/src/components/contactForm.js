17dabdeb6f2ba5fcd25d57ee4ac66d39def162f9
Jonathan Martin <oldschoolspaceempire@gmail.com>
2021-08-12 19:00:54 -0500

    switched out from formspree to emailjs with 500 emails a day

--- recipehubcode/src/components/contactForm.js
+++ recipehubcode/src/components/contactForm.js
@@ -1,39 +1,233 @@
 import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
-function ContactForm() {
-  const [state, handleSubmit] = useForm('https://formspree.io/f/mleaoeqy');
-  if (state.succeeded) {
-    return <p>Thanks for contacting RecipeHub!</p>;
+// Email validation
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+
+// Form validation
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  // Validate form errors being empty
+  Object.values(formErrors).forEach((val) => {
+    val.length > 0 && (valid = false);
+  });
+
+  // Validate the form was filled out
+  Object.values(rest).forEach((val) => {
+    val === '' && (valid = false);
+  });
+
+  return valid;
+};
+
+class ContactPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+      formErrors: {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+      },
+    };
   }
-  return (
-    <form
-      onSubmit={handleSubmit}
-      style={{
-        display: 'flex',
-        margin: 'auto',
-        width: '30%',
-        padding: '10px',
-        justifyContent: 'center',
-        border: '2px solid blue',
-        // eslint-disable-next-line no-dupe-keys
-        padding: '5px',
-        boxShadow: '2px 2px grey',
-      }}
-      action='https://formspree.io/f/mleaoeqy'
-      method='post'
-      header='Access-Control-Origin'
-    >
-      <label htmlFor='email'>Email Address</label>
-      <input id='email' type='email' name='email' />
-      <ValidationError prefix='Email' field='email' errors={state.errors} />
-      <label htmlFor='message'>Message</label>
-      <textarea id='message' name='message' />
-      <ValidationError prefix='Message' field='message' errors={state.errors} />
-      <button type='submit' disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
-  );
+
+  toastifySuccess() {
+    toast.success('Form sent!', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: 'submit-feedback success',
+    });
+  }
+
+  toastifyFail() {
+    toast.error('Form failed to send!', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: 'submit-feedback fail',
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formValid(this.state)) {
+      // Handle form validation success
+      const { name, email, subject, message } = this.state;
+
+      // Set template params
+      let templateParams = {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message,
+      };
+
+      emailjs.send(
+        'service_8w6ebcg',
+        'template_v0aj68g',
+        templateParams,
+        'user_MI3kmfd8ar1V9pRIZSkiR'
+      );
+
+      console.log(`
+        --SUBMITTING--
+        Name: ${name}
+        Email: ${email}
+        Subject: ${subject}
+        Message: ${message}
+      `);
+
+      this.toastifySuccess();
+      this.resetForm();
+    } else {
+      // Handle form validation failure
+      console.error('FORM INVALID - DISPLAY ERROR MESSAGE');
+      this.toastifyFail();
+    }
+  };
+
+  // Function to reset form
+  resetForm() {
+    this.setState({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    });
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let formErrors = { ...this.state.formErrors };
+
+    switch (name) {
+      case 'name':
+        formErrors.name = value.length < 1 ? 'Please enter your name.' : '';
+        break;
+      case 'email':
+        formErrors.email = emailRegex.test(value)
+          ? ''
+          : 'Please enter a valid email address.';
+        break;
+      case 'subject':
+        formErrors.subject = value.length < 1 ? 'Please enter a subject.' : '';
+        break;
+      case 'message':
+        formErrors.message = value.length < 1 ? 'Please enter a message' : '';
+        break;
+      default:
+        break;
+    }
+    this.setState({ formErrors, [name]: value });
+  };
+
+  render() {
+    const { formErrors } = this.state;
+
+    return (
+      <div className='ContactForm'>
+        <Form id='contact-form' onSubmit={this.handleSubmit} noValidate>
+          <div className='row'>
+            <div className='col-6'>
+              <input
+                type='text'
+                name='name'
+                value={this.state.name}
+                className={`form-control formInput ${
+                  formErrors.name.length > 0 ? 'error' : null
+                }`}
+                onChange={this.handleChange}
+                placeholder='Name'
+                noValidate
+              ></input>
+              {formErrors.name.length > 0 && (
+                <span className='errorMessage'>{formErrors.name}</span>
+              )}
+            </div>
+
+            <div className='col-6'>
+              <input
+                type='email'
+                name='email'
+                value={this.state.email}
+                className={`form-control formInput ${
+                  formErrors.email.length > 0 ? 'error' : null
+                }`}
+                onChange={this.handleChange}
+                placeholder='Email'
+                noValidate
+              ></input>
+              {formErrors.email.length > 0 && (
+                <span className='errorMessage'>{formErrors.email}</span>
+              )}
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col-6'>
+              <input
+                type='subject'
+                name='subject'
+                value={this.state.subject}
+                className={`form-control formInput ${
+                  formErrors.subject.length > 0 ? 'error' : null
+                }`}
+                onChange={this.handleChange}
+                placeholder='Subject'
+                noValidate
+              ></input>
+              {formErrors.subject.length > 0 && (
+                <span className='errorMessage'>{formErrors.subject}</span>
+              )}
+            </div>
+
+            <div className='col-6'>
+              <textarea
+                rows='5'
+                name='message'
+                value={this.state.message}
+                className={`form-control formInput ${
+                  formErrors.message.length > 0 ? 'error' : null
+                }`}
+                onChange={this.handleChange}
+                placeholder='Message'
+                noValidate
+              ></textarea>
+              {formErrors.message.length > 0 && (
+                <span className='errorMessage'>{formErrors.message}</span>
+              )}
+            </div>
+          </div>
+          <Button className='submit-btn' type='submit'>
+            Submit
+          </Button>
+        </Form>
+        <ToastContainer />
+      </div>
+    );
+  }
 }
-export default ContactForm;
+
+export default ContactPage;
--- recipehubcode/src/pages/contactPage.js
+++ recipehubcode/src/pages/contactPage.js
@@ -1,11 +1,24 @@
 import ContactForm from '../components/contactForm';
-import React from 'react';
+import React, { useState } from 'react';
+import { curRentState } from '../assets/pageTitles';
+import { Container } from 'react-bootstrap';
 
 const ContactPage = () => {
+  //eslint-disable-next-line
+  const [currentState, setCurrentState] = useState({ ...curRentState });
   return (
-    <div>
+    <Container
+      style={{
+        border: '2px solid blue',
+        padding: '5px',
+        boxShadow: '2px 2px grey',
+      }}
+    >
+      <h1 style={{ display: 'flex', justifyContent: 'center' }}>
+        {currentState.contact.title}
+      </h1>
       <ContactForm />
-    </div>
+    </Container>
   );
 };
 
